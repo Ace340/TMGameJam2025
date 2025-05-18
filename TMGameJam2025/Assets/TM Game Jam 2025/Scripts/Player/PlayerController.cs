891c4c2cc7cb628bbc4e2076825175b68113ae9b
Roberto Hernandez <rhernandez@rayblast.com>
2025-05-18 12:45:08 -0500

    Updated:  Player Movement & Camera

--- TMGameJam2025/Assets/TM Game Jam 2025/Scripts/Player/PlayerController.cs
+++ TMGameJam2025/Assets/TM Game Jam 2025/Scripts/Player/PlayerController.cs
@@ -1,30 +1,28 @@
-using NUnit.Framework.Constraints;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
-
     [SerializeField] Animator animator;
 
     private PlayerInputActions controls;
     private Vector2 moveInput;
     private Rigidbody rb;
     public float moveSpeed = 5f;
+
     public float rotationSpeed = 180f;
 
     private Vector3 lastPosition;
-
-    void Start()
-    {
-        lastPosition = transform.position;
-    }
 
     void Awake()
     {
         controls = new PlayerInputActions();
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        lastPosition = transform.position;
     }
 
     void OnEnable()
@@ -41,10 +39,6 @@ public class PlayerController : MonoBehaviour
         controls.Player.Move.canceled -= OnMove;
     }
 
-    /// <summary>
-    /// Callback reads the input value from the Input System (MOVE)
-    /// </summary>
-    /// <param name="context"></param>
     void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
@@ -53,48 +47,72 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         MovePlayer();
+        RotateTowardsMouse();
         AnimatePlayer();
-
         lastPosition = transform.position;
     }
 
-
-
     private void MovePlayer()
     {
+        Vector3 moveDirection = new Vector3(-moveInput.y, 0f, moveInput.x);
 
-        float moveAxis = moveInput.y; // W: 1, S: -1
-        if (Mathf.Abs(moveAxis) > 0.1f)
+        if (moveDirection.sqrMagnitude > 0.01f)
         {
-            Vector3 moveDirection = transform.forward * moveAxis;
+            moveDirection = moveDirection.normalized;
             Vector3 moveVelocity = moveDirection * moveSpeed;
             rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
         }
-
-        float rotateAxis = moveInput.x; // A: -1, D: 1
-        if (Mathf.Abs(rotateAxis) > 0.1f)
-        {
-            float rotationDelta = rotateAxis * rotationSpeed * Time.fixedDeltaTime;
-            transform.Rotate(0f, rotationDelta, 0f);
-        }
     }
+
+
+    //Move Player Juanki
+    // private void MovePlayer()
+    // {
+
+    //     float moveAxis = moveInput.y; // W: 1, S: -1
+    //     if (Mathf.Abs(moveAxis) > 0.1f)
+    //     {
+    //         Vector3 moveDirection = transform.forward * moveAxis;
+    //         Vector3 moveVelocity = moveDirection * moveSpeed;
+    //         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    //     }
+
+    //     float rotateAxis = moveInput.x; // A: -1, D: 1
+    //     if (Mathf.Abs(rotateAxis) > 0.1f)
+    //     {
+    //         float rotationDelta = rotateAxis * rotationSpeed * Time.fixedDeltaTime;
+    //         transform.Rotate(0f, rotationDelta, 0f);
+    //     }
+    // }
 
     private void AnimatePlayer()
     {
-        if (CalculatePlayerSpeed() > 0)
-        {
-            animator.SetBool("IsWalking", true);
-        }
-        else
-        {
-
-            animator.SetBool("IsWalking", false);
-        }
+        animator.SetBool("IsWalking", CalculatePlayerSpeed() > 0);
     }
 
     private float CalculatePlayerSpeed()
     {
-        float currentSpeed = (transform.position - lastPosition).magnitude / Time.fixedDeltaTime;
-        return currentSpeed;
+        return (transform.position - lastPosition).magnitude / Time.fixedDeltaTime;
+    }
+
+
+
+    private void RotateTowardsMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero); // y=0
+
+        if (groundPlane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            Vector3 direction = hitPoint - transform.position;
+            direction.y = 0f;
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 20f * Time.deltaTime);
+            }
+        }
     }
 }
