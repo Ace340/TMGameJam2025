046533e43f4a2a5c9b4bcfb0019b168c787fd5f0
Roberto Hernandez <rhernandez@rayblast.com>
2025-05-18 13:01:14 -0500

    Added: Weapon class

---
+++ TMGameJam2025/Assets/TM Game Jam 2025/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Weapon : MonoBehaviour
+{
+    [Header("Raycast Settings")]
+    [SerializeField] private Transform shootOrigin;
+    [SerializeField] private float shootDistance = 100f;
+    [SerializeField] private LayerMask hitLayers;
+
+    void Update()
+    {
+        // Dispara solo cuando se hace click izquierdo
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            ShootRay();
+        }
+    }
+
+    private void ShootRay()
+    {
+        Vector3 origin = shootOrigin.position;
+        Vector3 direction = shootOrigin.forward;
+
+        Debug.DrawRay(origin, direction * shootDistance, Color.green, 1f);
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, shootDistance))
+        {
+            Debug.Log($"Hit: {hit.collider.name}");
+
+        }
+    }
+}
