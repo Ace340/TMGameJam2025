184eb675baa2ee87785d91fbe6a83ec57f014cbc
Roberto Hernandez <rhernandez@rayblast.com>
2025-05-18 11:41:53 -0500

    Added: Player Walk Animation

--- TMGameJam2025/Assets/TM Game Jam 2025/Scripts/Player/PlayerController.cs
+++ TMGameJam2025/Assets/TM Game Jam 2025/Scripts/Player/PlayerController.cs
@@ -1,33 +1,41 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private PlayerInputActions controls; 
-    private Vector2 moveInput; 
-    private Rigidbody rb; 
-    public float moveSpeed = 5f; 
-    public float rotationSpeed = 180f; 
+
+
+    [SerializeField] Animator animator;
+
+    private PlayerInputActions controls;
+    private Vector2 moveInput;
+    private Rigidbody rb;
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 180f;
+
+    private Vector3 lastPosition;
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
 
     void Awake()
     {
-        // Input system
         controls = new PlayerInputActions();
         rb = GetComponent<Rigidbody>();
     }
 
     void OnEnable()
     {
-        // Action Map
         controls.Player.Enable();
-        
         controls.Player.Move.performed += OnMove;
         controls.Player.Move.canceled += OnMove;
     }
 
     void OnDisable()
     {
-        // Disable while no inputs
         controls.Player.Disable();
         controls.Player.Move.performed -= OnMove;
         controls.Player.Move.canceled -= OnMove;
@@ -44,22 +52,49 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Forward and Backward Movement (W/S)
+        MovePlayer();
+        AnimatePlayer();
+
+        lastPosition = transform.position;
+    }
+
+
+
+    private void MovePlayer()
+    {
+
         float moveAxis = moveInput.y; // W: 1, S: -1
         if (Mathf.Abs(moveAxis) > 0.1f)
         {
-            // Moves forward/backward in the direction the player is facing
             Vector3 moveDirection = transform.forward * moveAxis;
             Vector3 moveVelocity = moveDirection * moveSpeed;
             rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
         }
 
-        // Rotation (A/D)
         float rotateAxis = moveInput.x; // A: -1, D: 1
         if (Mathf.Abs(rotateAxis) > 0.1f)
         {
             float rotationDelta = rotateAxis * rotationSpeed * Time.fixedDeltaTime;
             transform.Rotate(0f, rotationDelta, 0f);
         }
+    }
+
+    private void AnimatePlayer()
+    {
+        if (CalculatePlayerSpeed() > 0)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+
+            animator.SetBool("IsWalking", false);
+        }
+    }
+
+    private float CalculatePlayerSpeed()
+    {
+        float currentSpeed = (transform.position - lastPosition).magnitude / Time.fixedDeltaTime;
+        return currentSpeed;
     }
 }
