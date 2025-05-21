3dbad3b8b0222d6126c693b7ccc9070de6c23cad
Ace340 <53065538+Ace340@users.noreply.github.com>
2025-05-20 22:36:32 -0400

    Added: IntroScene

---
+++ TMGameJam2025/Assets/TM Game Jam 2025/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _quitButton;
+    [SerializeField] private Button _aboutButton;
+}
