b4ccc22a6a07f571e9e63a887367c4d37aef3979
Ace340 <53065538+Ace340@users.noreply.github.com>
2025-05-21 21:13:37 -0400

    Added: Main Menu and LoadingScene

--- TMGameJam2025/Assets/TM Game Jam 2025/Scripts/UI/MainMenuUI.cs
+++ TMGameJam2025/Assets/TM Game Jam 2025/Scripts/UI/MainMenuUI.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
@@ -6,4 +7,16 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _quitButton;
     [SerializeField] private Button _aboutButton;
+
+    private void Awake()
+    {
+        _playButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.MainScene);
+        });
+        _quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
 }
