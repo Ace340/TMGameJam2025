b445fecaa53acba02ac45957b67889b96b34b620
Ace340 <53065538+Ace340@users.noreply.github.com>
2025-05-31 11:41:34 -0400

    Fixed: Walls bugs and coin bugs

--- TMGameJam2025/Assets/TM Game Jam 2025/Scripts/Coin.cs
+++ TMGameJam2025/Assets/TM Game Jam 2025/Scripts/Coin.cs
@@ -1,33 +1,31 @@
 using UnityEngine;
-using System;
-using System.Collections;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private AudioSource coinSound; 
 
-
-
-    [SerializeField] GameObject coin;
-    [SerializeField] AudioSource coinsound;
-
-
-
-
-
-
-
-    void OnTriggerEnter(Collider other)
-
-
-
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("hay colision");
-        if (other.gameObject.CompareTag("Player"))
+        Debug.Log("Hay colisión");
+        if (other.CompareTag("Player"))
         {
             ScoreManager.Instance.AddScore(50);
-            coin.SetActive(false);
-            coinsound.Play();
+
+            // Play Sound
+            if (coinSound != null)
+            {
+                coinSound.Play();
+            }
+
+            if (coinSound != null && coinSound.clip != null)
+            {
+                Destroy(gameObject, coinSound.clip.length);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
         }
     }
-
 }
